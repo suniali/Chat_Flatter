036cd7dd56f52a851261a270910fa3ab5c369149
ali <alimatroky@gmail.com>
2019-01-22 12:04:35 +0330

    create chat_message.dart and some changes in chat screen

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -13,25 +13,28 @@ class ChatScreenState extends State<ChatScreen>
       _textEditingController.clear();
   }
   Widget _textComposerWidget(){
-    return  Container(
-      margin: EdgeInsets.symmetric(horizontal: 8.0),
-      child:Row(children: <Widget>[
-        Flexible(
-            child: TextField(
-              controller: _textEditingController,
-              onSubmitted: _handleSubmited,
-              decoration: InputDecoration.collapsed(
-                hintText: "Send Message...",
+    return  IconTheme(
+      data: IconThemeData(color: Colors.blue),
+          child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child:Row(children: <Widget>[
+          Flexible(
+              child: TextField(
+                controller: _textEditingController,
+                onSubmitted: _handleSubmited,
+                decoration: InputDecoration.collapsed(
+                  hintText: "Send Message...",
+                ),
               ),
-            ),
-        ),
-        Container(
-          child: IconButton(
-            icon: Icon(Icons.send),
-            onPressed: ()=>_handleSubmited(_textEditingController.text)
           ),
-        )
-      ],)
+          Container(
+            child: IconButton(
+              icon: Icon(Icons.send),
+              onPressed: ()=>_handleSubmited(_textEditingController.text)
+            ),
+          )
+        ],)
+      ),
     );
   }
   @override
